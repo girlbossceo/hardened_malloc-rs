950a72539bd0e220a5c55bbe5b71a1453b2f1cca
strawberry <strawberry@pupbrain.dev>
2023-11-12 18:24:45 -0500

    drop nightly/experimental AllocError/Allocator, make this actually build, bump sys crate
    Signed-off-by: strawberry <strawberry@pupbrain.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -2,42 +2,30 @@
 
 extern crate hardened_malloc_sys as ffi;
 
-use core::alloc::{GlobalAlloc, Layout, Allocator, AllocError};
-use core::ffi::{c_int, c_void};
+use core::alloc::{GlobalAlloc, Layout};
+use core::ffi::c_void;
 use ffi::*;
 
 pub struct HardenedMalloc;
 
 unsafe impl GlobalAlloc for HardenedMalloc {
     #[inline]
-    unsafe fn alloc(&self, layout: Layout) -> Result<*mut u8, AllocError> {
-        let ptr = h_malloc(layout.size());
-        if ptr.is_null() {
-            return Err(AllocError);
-        }
-        Ok(ptr)
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        h_malloc(layout.size()) as *mut u8
     }
 
     #[inline]
-    unsafe fn alloc_zeroed(&self, layout: Layout) -> Result<*mut u8, AllocError> {
-        let ptr = h_calloc(layout.size(), 1);
-        if ptr.is_null() {
-            return Err(AllocError);
-        }
-        Ok(ptr)
+    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
+        h_calloc(layout.size(), 1) as *mut u8
     }
 
     #[inline]
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
         h_free(ptr as *mut c_void);
     }
 
     #[inline]
-    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> Result<*mut u8, AllocError> {
-        let ptr = h_realloc(ptr, new_size);
-        if ptr.is_null() {
-            return Err(AllocError);
-        }
-        Ok(ptr)
+    unsafe fn realloc(&self, ptr: *mut u8, _layout: Layout, size: usize) -> *mut u8 {
+        h_realloc(ptr as *mut c_void, size) as *mut u8
     }
 }
