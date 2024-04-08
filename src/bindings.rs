04ca5722961e41741086f1865210e09aeaa7a128
Evan Huff <ehuff007@gmail.com>
2024-04-08 16:58:00 -0400

    chore: add rustfmt

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,10 +1,10 @@
-use core::ffi::{c_void};
+use core::ffi::c_void;
 
 #[allow(dead_code)]
 extern "C" {
-    /* C standard */
-    pub fn malloc(size: usize) -> *mut c_void;
-    pub fn calloc(nmemb: usize, size: usize) -> *mut c_void;
-    pub fn realloc(ptr: *mut c_void, size: usize) -> *mut c_void;
-    pub fn free(ptr: *mut c_void);
+	/* C standard */
+	pub fn malloc(size: usize) -> *mut c_void;
+	pub fn calloc(nmemb: usize, size: usize) -> *mut c_void;
+	pub fn realloc(ptr: *mut c_void, size: usize) -> *mut c_void;
+	pub fn free(ptr: *mut c_void);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,27 @@
 #![no_std]
 
 mod bindings;
-pub use bindings::{malloc, calloc, free, realloc};
-use core::alloc::{GlobalAlloc, Layout};
-use core::ffi::c_void;
+use core::{
+	alloc::{GlobalAlloc, Layout},
+	ffi::c_void,
+};
+
+pub use bindings::{calloc, free, malloc, realloc};
 
 pub struct HardenedMalloc;
 
 unsafe impl GlobalAlloc for HardenedMalloc {
-    #[inline]
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        malloc(layout.size()) as *mut u8
-    }
-    #[inline]
-    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        calloc(layout.size(), 1) as *mut u8
-    }
-    #[inline]
-    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-       free(ptr as *mut c_void);
-    }
-    #[inline]
-    unsafe fn realloc(&self, ptr: *mut u8, _layout: Layout, size: usize) -> *mut u8 {
-       realloc(ptr as *mut c_void, size) as *mut u8
-    }
+	#[inline]
+	unsafe fn alloc(&self, layout: Layout) -> *mut u8 { malloc(layout.size()) as *mut u8 }
+
+	#[inline]
+	unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 { calloc(layout.size(), 1) as *mut u8 }
+
+	#[inline]
+	unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) { free(ptr as *mut c_void); }
+
+	#[inline]
+	unsafe fn realloc(&self, ptr: *mut u8, _layout: Layout, size: usize) -> *mut u8 {
+		realloc(ptr as *mut c_void, size) as *mut u8
+	}
 }
